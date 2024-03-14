91109170f7cfbfd97c6fb610e50b13a7f306ea32
Viet Bui <buiminhviet.2002@gmail.com>
2024-03-14 09:17:09 -0500

    Updated requirements.txt and the test.py inside infra works

--- roadrunner/infra/test.py
+++ roadrunner/infra/test.py
@@ -2,7 +2,7 @@ from audiototext import audio_to_text
 
 
 def main():
-    file_location = "audio/Perfection.mp3"
+    file_location = "./roadrunner/infra/audio/Perfection.mp3"
     result = audio_to_text('local', file_location, True)
 
 

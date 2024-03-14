e9fa6b4faee3ca8c1f4740300b07b17eb9620d8a
StephenKodua <koduastephen9@gmail.com>
2024-03-14 03:13:27 -0500

    Added UI for recording, clear, stop on Homescreen

--- roadrunner/client/constants/index.js
+++ roadrunner/client/constants/index.js
@@ -1,21 +1,38 @@
-export
-const dummyMessages = [
+export const dummyMessages = [
     {
         role: 'user',
-        content: 'How are you?'
-
+        content: 'Hey there! How are you doing today?'
     },
     {
         role: 'assistant',
-        content: "I'm fine, Howgmay i help you today."
+        content: "Hi! I'm doing great, thank you for asking! How can I assist you today?"
     },
     {
         role: 'user',
-        content: 'create an image of a dog playing with cat'
-        
+        content: 'I need a little break from work. Can you tell me a joke?'
     },
     {
         role: 'assistant',
-        content: 'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300-jpeg' 
+        content: "Sure! Here's one: Why don't scientists trust atoms? Because they make up everything!"
+    },
+    {
+        role: 'user',
+        content: 'Haha, that was a good one! Can you show me a cute cat iamge?'
+    },
+    {
+        role: 'assistant',
+        content: 'Of course! How about this?'
+    },
+    {
+        role: 'assistant',
+        content: 'https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg'
+    },
+    {
+        role: 'user',
+        content: 'Haha, that\'s cute! Thanks for the laugh!'
+    },
+    {
+        role: 'assistant',
+        content: "You're welcome! Feel free to ask if you need anything else."
     }
-]
+];
--- roadrunner/client/screens/HomeScreen.js
+++ roadrunner/client/screens/HomeScreen.js
@@ -1,14 +1,35 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { useEffect, useState } from "react";
-import Features from "../components/Features"
+import Features from "../components/Features";
+
+import { dummyMessages } from "../constants";
 
 export default function HomeScreen() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(false);
+
+  const clear = () => {
+    setMessages([]);
+  }; 
+
+  const stopSpeaking = () => {
+    setSpeaking(false);
+  }
+
   return (
     <View className=" flex-1 bg-white">
       <SafeAreaView className="flex-1 flex mx-5">
@@ -22,7 +43,111 @@ export default function HomeScreen() {
 
         {/* features || messages */}
 
-        {messages.length ? 0(<View></View>) : <Features />}
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{ fontSize: wp(5) }}
+              className="text-gray-700 font-semibold ml-1"
+            >
+              Assitant
+            </Text>
+
+            <View
+              style={{ height: hp(58) }}
+              className="bg-neutral-200 rounded-3xl p-4"
+            >
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}
+              >
+                {messages.map((message, index) => {
+                  if (message.role == "assistant") {
+                    if (message.content.includes("https")) {
+                      // Render image
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{ uri: message.content }}
+                              style={{
+                                height: wp(60),
+                                width: wp(60),
+                                borderRadius: wp(5),
+                              }}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      return (
+                        <View
+                          key={index}
+                          style={{ width: wp(70) }}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    // Render user input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{ width: wp(70) }}
+                          className="bg-white rounded-xl p-2 rounded-tr-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
+        {/** recording, clear and stop buttons */}
+        <View className="flex justify-center items-center">
+          {recording ? (
+            <TouchableOpacity>
+              <Image
+                className="rounded-full"
+                source={require("../asset/images/voiceRecording.gif")}
+                style={{ width: hp(10), height: hp(10) }}
+              />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity>
+              <Image
+                className="rounded-full"
+                source={require("../asset/images/recordingIcon.png")}
+                style={{ width: hp(7), height: hp(7) }}
+              />
+            </TouchableOpacity>
+          )}
+
+          {messages.length > 0 && (
+            <TouchableOpacity
+              onPress={clear}
+              className="bg-neutral-400 rounded-3xl p-2 absolute right-10"
+            >
+              <Text className="text-white font-semibold">Clear</Text>
+            </TouchableOpacity>
+          )}
+
+          {speaking && (
+            <TouchableOpacity
+              onPress={stopSpeaking}
+              className="bg-red-400 rounded-3xl p-2 absolute left-10"
+            >
+              <Text className="text-white font-semibold">Stop</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </SafeAreaView>
     </View>
   );

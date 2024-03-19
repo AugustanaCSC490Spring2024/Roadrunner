3349704de86d271bf87cc003eaee30d2e5733cf6
Joshua-fosu <joshuafosu119@gmail.com>
2024-03-19 04:05:12 -0400

    Added email validator util

--- roadrunner/infra/routes/auth.py
+++ roadrunner/infra/routes/auth.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    user_data = request.form
+    if request.method == 'POST':
+        username = user_data.get('username')
+        email = user_data.get('email')
+        password = user_data.get('password')
+             
+
+@auth.route('/logout', methods=['GET', 'POST'])
+def logout():
     pass

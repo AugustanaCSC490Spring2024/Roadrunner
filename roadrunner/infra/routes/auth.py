86768065e4cb29cc91c457011ff4daf1750a19f4
Joshua-fosu <joshuafosu119@gmail.com>
2024-03-19 04:17:19 -0400

    Added password validation algorithm and strength check

--- roadrunner/infra/routes/auth.py
+++ roadrunner/infra/routes/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request
+from emailvalidator import user_validate_email
+from password_hash_algorithm import check_password_strength
 
 auth = Blueprint('auth', __name__)
 
@@ -13,7 +15,11 @@ def signup():
         username = user_data.get('username')
         email = user_data.get('email')
         password = user_data.get('password')
-             
+        if user_validate_email(password) is not None and check_password_strength(password):
+            print('Ready for sign up')
+        else:
+            print('check user email or password strength')
+            
 
 @auth.route('/logout', methods=['GET', 'POST'])
 def logout():
---
+++ roadrunner/infra/utils/password_hash_algorithm.py
@@ -0,0 +1,19 @@
+import hashlib
+from password_strength import PasswordPolicy, PasswordStats
+
+policy = PasswordPolicy.from_names(
+    length=8,  # min length: 8
+    uppercase=2,  # need min. 2 uppercase letters
+    numbers=2,  # need min. 2 digits
+    special=2,  # need min. 2 special characters
+    nonletters=2,  # need min. 2 non-letter characters (digits, specials, anything)
+)
+
+def hash_password(password):
+   password_bytes = password.encode('utf-8')
+   hash_object = hashlib.sha256(password_bytes)
+   return hash_object.hexdigest()
+
+def check_password_strength(password):
+    return PasswordStats(password).strength() >= 0.65
+

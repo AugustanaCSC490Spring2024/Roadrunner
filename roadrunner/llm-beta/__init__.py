4d85f4ff1409d48a94b538e1af3e9f95de5b8fbf
Joshua-fosu <joshuafosu119@gmail.com>
2024-04-09 14:59:25 -0400

    Refactored train.py into multiple independent python classes and files

--- roadrunner/llm-beta/__init__.py
+++ roadrunner/llm-beta/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
+from .bigrammodel import BigramLanguageModel
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = BigramLanguageModel()
+m = model.to(device)
+
 
 def create_app():
     app = Flask(__name__)
---
+++ roadrunner/llm-beta/block.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+from .multiheadattention import MultiHeadAttention
+from .feedforward import FeedFoward
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x

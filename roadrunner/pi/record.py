6c6cc335a60255088f594c0326c6b3a45316714a
Viet Bui <buiminhviet.2002@gmail.com>
2024-04-18 11:53:22 -0500

    Updated record file for Pi

--- roadrunner/pi/record.py
+++ roadrunner/pi/record.py
@@ -2,23 +2,26 @@ import pyaudio
 import wave
 from pydub import AudioSegment
 import os
+import requests
 
 # Set parameters
 RATE = 48000
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RECORD_SECONDS = 10
+RECORD_SECONDS = 60
 FOLDER_LOCATION = "audio/"
+
 
 def countfile(directory):
     return os.listdir(directory)
+
 
 count = 0
 while count < 100:
     files = countfile(FOLDER_LOCATION)
     full_path = ["audio/{0}".format(x) for x in files]
-    if len(files) >= 10:
+    if len(files) >= 2:
         oldest = min(full_path, key=os.path.getctime)
         try:
             os.remove(os.path.abspath(oldest))
@@ -26,7 +29,7 @@ while count < 100:
             print("File cannot be removed")
 
     WAVE_OUTPUT_FILENAME = FOLDER_LOCATION + "output" + str(count) + ".wav"
-    MP3_OUTPUT_FILENAME = FOLDER_LOCATION + "output"+ str(count) + ".mp3"
+    MP3_OUTPUT_FILENAME = FOLDER_LOCATION + "output" + str(count) + ".mp3"
     # Initialize PyAudio
     p = pyaudio.PyAudio()
 
@@ -67,4 +70,15 @@ while count < 100:
     # sound.export(MP3_OUTPUT_FILENAME, format="mp3")
 
     print(f"Audio recording saved as {MP3_OUTPUT_FILENAME}")
+
+    # Curl process happens here
+    url = 'https://remotely-many-tortoise.ngrok-free.app/audio'
+    files = {'audio': open(f'{WAVE_OUTPUT_FILENAME}', 'rb')}
+    data = {'user_id': '1'}
+
+    response = requests.post(url, files=files, data=data)
+
+    # print the response
+    print(response.text)
+
     count = count + 1

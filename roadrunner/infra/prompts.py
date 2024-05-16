d4597a7105bab325e9042c69944cbcc3068b86ac
tthuwng <tronghung199@gmail.com>
2024-05-16 10:59:32 -0500

    fix /chat concat lists

--- roadrunner/infra/prompts.py
+++ roadrunner/infra/prompts.py
@@ -1,7 +1,9 @@
+from typing import List
+
 nl = "\n"
 
 
-def system_prompt(records) -> str:
+def system_prompt(records: List[str]) -> str:
     return f"""You are the most helpful and advanced personal assistant ever, helping the user navigate through life. 
       User is asking you questions, and you answer them with the best of your ability.
       You have access to some of their records, to help you answer their question in a more personalized way.

461f327bb8f48f004ea24a4bbc5bcc4529e51675
tthuwng <tronghung199@gmail.com>
2024-05-16 10:59:36 -0500

    add cors

--- roadrunner/infra/main.py
+++ roadrunner/infra/main.py
@@ -1,6 +1,8 @@
-import uvicorn
 import os
+
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db.db import database, engine, metadata
 from .db.models import Base
@@ -11,6 +13,17 @@ from .routes.conversation import router as conversation_router
 from .routes.user import router as user_router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
 app.include_router(audio_router)
@@ -37,7 +50,7 @@ async def shutdown():
     await database.disconnect()
 
 
-port = int(os.environ.get('PORT', 8080))
+port = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=port)
